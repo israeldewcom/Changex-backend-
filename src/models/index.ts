368a28d29192452877c6b4333dafaeb08ba8a057
israeldewcom <haddockthor@gmail.com>
2026-04-05 13:14:27 +0100

    Create index.ts

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+export { User } from './User';
+export { Course } from './Course';
+export { Enrollment } from './Enrollment';
+export { Transaction } from './Transaction';
+export { Referral } from './Referral';
+export { Certificate } from './Certificate';
+export { Marketplace, Order } from './Marketplace';
+export { Job, Application } from './Job';
+export { Post, Comment } from './Post';
+export { Notification } from './Notification';
+export { Badge, UserBadge } from './Badge';
+export { AuditLog } from './AuditLog';
+export { Coupon } from './Coupon';
+export { Conversation, ChatMessage } from './Chat';
+export { Review } from './Review';
