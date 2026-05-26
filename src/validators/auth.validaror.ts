78a0fa74e166e33d6ac4ed2bb18c4ae1a1645452
israeldewcom <haddockthor@gmail.com>
2026-05-26 15:05:39 +0100

    Create auth.validaror.ts

---
+++ src/validators/auth.validaror.ts
@@ -0,0 +1,24 @@
+// File: src/validators/auth.validator.ts
+import Joi from 'joi';
+
+export const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  referralCode: Joi.string().optional(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const forgotPasswordSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+export const resetPasswordSchema = Joi.object({
+  token: Joi.string().required(),
+  newPassword: Joi.string().min(8).required(),
+});
