f6ed85808253451a3d676e4dc649566a2dd5ac79
israeldewcom <haddockthor@gmail.com>
2026-05-27 09:40:59 +0100

    Create payment.routes.ts

---
+++ src/routes/payment.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as paymentController from '../controllers/payment.controller.js';
+import { authenticate } from '../middlewares/auth.js';
+
+const router = Router();
+router.post('/initialize', authenticate, paymentController.payForCourse);
+router.post('/verify', authenticate, paymentController.verifyPayment);
+export default router;
