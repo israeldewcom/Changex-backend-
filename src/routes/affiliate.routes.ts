fc8e82314abd0eaffc6b3e460ee96d33797ce172
israeldewcom <haddockthor@gmail.com>
2026-05-27 14:18:42 +0100

    Update affiliate.routes.ts

--- src/routes/affiliate.routes.ts
+++ src/routes/affiliate.routes.ts
@@ -1,3 +1,4 @@
+// src/routes/affiliate.routes.ts
 import { Router } from 'express';
 import * as affiliateController from '../controllers/affiliate.controller.js';
 import { authenticate } from '../middlewares/auth.js';
@@ -8,5 +9,7 @@ router.get('/track/:code', affiliateController.trackAffiliateClick);
 router.use(authenticate);
 router.post('/accept', affiliateController.acceptAffiliateOffer);
 router.get('/my-links', affiliateController.getMyLinks);
+router.get('/stats', affiliateController.getAffiliateStats);
+router.get('/leaderboard', affiliateController.getAffiliateLeaderboard);
 
 export default router;
