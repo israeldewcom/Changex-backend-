5c61352e9197c5f8534f661cefdf3a49211b45a4
israeldewcom <haddockthor@gmail.com>
2026-05-27 11:27:14 +0100

    Update payment.routes.ts

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -3,6 +3,8 @@ import * as paymentController from '../controllers/payment.controller.js';
 import { authenticate } from '../middlewares/auth.js';
 
 const router = Router();
+
 router.post('/initialize', authenticate, paymentController.payForCourse);
 router.post('/verify', authenticate, paymentController.verifyPayment);
+
 export default router;
