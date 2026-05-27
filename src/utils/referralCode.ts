45f47553c344e891eed401047134f98b2e67fedc
israeldewcom <haddockthor@gmail.com>
2026-05-27 14:21:17 +0100

    Update referralCode.ts

--- src/utils/referralCode.ts
+++ src/utils/referralCode.ts
@@ -1,5 +1,6 @@
-// File: src/utils/referralCode.ts
+// src/utils/referralCode.ts
 import { customAlphabet } from 'nanoid';
+
 const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 8);
 
 export const generateReferralCode = (): string => {
