e1e7465514ae328685ee9f34f7374d67bb6a4c1f
israeldewcom <haddockthor@gmail.com>
2026-05-27 09:43:03 +0100

    Create errorHandler.ts

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from '../utils/logger.js';
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  res.status(err.status || 500).json({ success: false, message: err.message });
+};
