3017e0314f1e6f7285d83e2a3cfa88daa5842a9f
israeldewcom <haddockthor@gmail.com>
2026-05-27 12:40:48 +0100

    Create sanitize.ts

---
+++ src/middlewares/sanitize.ts
@@ -0,0 +1,9 @@
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+export const sanitizeHtml = (dirty: string): string => {
+  return DOMPurify.sanitize(dirty);
+};
