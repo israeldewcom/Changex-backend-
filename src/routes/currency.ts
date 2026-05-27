cb327335cc47f57c65db989cd8eeb675a0d57980
israeldewcom <haddockthor@gmail.com>
2026-05-27 07:15:47 +0100

    Delete src/routes/currency.ts

--- src/routes/currency.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { CurrencyController } from '../controllers/CurrencyController';
-
-const router = Router();
-const currencyController = new CurrencyController();
-
-router.get('/rates', currencyController.getRates);
-
-export default router;
