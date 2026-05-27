231dee6ae42d19634db3c0517a5aefc5516916d0
israeldewcom <haddockthor@gmail.com>
2026-05-27 12:39:57 +0100

    Update upload.ts

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,5 +1,7 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
-const storage = new CloudinaryStorage({ cloudinary, params: { folder: 'changex' } });
-export const upload = multer({ storage, limits: { fileSize: 100 * 1024 * 1024 } });
+
+const storage = multer.memoryStorage();
+export const upload = multer({ 
+  storage, 
+  limits: { fileSize: 100 * 1024 * 1024 } // 100MB
+});
