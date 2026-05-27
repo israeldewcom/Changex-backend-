0b7965302754b0fc637f636de96c311783f36787
israeldewcom <haddockthor@gmail.com>
2026-05-27 14:22:53 +0100

    Create Feedback.ts

---
+++ src/models/Feedback.ts
@@ -0,0 +1,17 @@
+// src/models/Feedback.ts
+import mongoose, { Schema, Document } from 'mongoose';
+export interface IFeedback extends Document {
+  message: string;
+  email?: string;
+  userId?: mongoose.Types.ObjectId;
+  url?: string;
+  userAgent?: string;
+}
+const FeedbackSchema = new Schema<IFeedback>({
+  message: { type: String, required: true },
+  email: String,
+  userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  url: String,
+  userAgent: String,
+}, { timestamps: true });
+export default mongoose.model<IFeedback>('Feedback', FeedbackSchema);
