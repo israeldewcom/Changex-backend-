ed6854f32d3aaf8a02eacc2cfba48df5d842d17a
israeldewcom <haddockthor@gmail.com>
2026-05-27 14:15:32 +0100

    Update payment.routes.ts

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,10 +1,14 @@
+// src/routes/payment.routes.ts
 import { Router } from 'express';
 import * as paymentController from '../controllers/payment.controller.js';
 import { authenticate } from '../middlewares/auth.js';
 
 const router = Router();
 
-router.post('/initialize', authenticate, paymentController.payForCourse);
-router.post('/verify', authenticate, paymentController.verifyPayment);
+router.post('/initialize-paystack', authenticate, paymentController.initializeTransaction);
+router.post('/verify-paystack', authenticate, paymentController.verifyTransaction);
+router.post('/subscribe', authenticate, paymentController.subscribe);
+router.get('/transactions', authenticate, paymentController.getTransactions);
+router.post('/withdraw', authenticate, paymentController.withdraw);
 
 export default router;
