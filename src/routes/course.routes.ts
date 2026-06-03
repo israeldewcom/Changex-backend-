5c11e4ed2c589d5532ba1c8aed2fa0276956d08c
israeldewcom <haddockthor@gmail.com>
2026-06-03 18:28:35 +0100

    Add certificate download route to course routes

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import * as courseController from '../controllers/course.controller.js';
+import * as certificateController from '../controllers/certificate.controller.js';
 import { authenticate } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.get('/', courseController.getPublishedCourses);
-router.get('/my/enrollments', authenticate, courseController.getUserEnrollments);
 router.get('/:id', courseController.getCourse);
+router.get('/my/enrollments', authenticate, courseController.getUserEnrollments);
 router.post('/:id/enroll', authenticate, courseController.enrollCourse);
 router.post('/:id/lessons/:lessonId/progress', authenticate, courseController.updateLessonProgress);
 router.post('/:id/rate', authenticate, courseController.rateCourse);
+router.get('/:courseId/certificate/download', authenticate, certificateController.downloadCertificate);
 
 export default router;
